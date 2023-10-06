677cc455822ccdb573645348e7340227069367ec
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-06 21:41:48 +0900

    Add: formrun

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import Formrun from '@/app/components/page/contact/Formrun';
+
+export default function Page() {
+  return (
+    <div className="min-h-screen">
+      <Formrun />
+    </div>
+  );
+}
