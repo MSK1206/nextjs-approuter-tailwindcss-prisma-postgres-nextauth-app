4bed8cbe0524d6a79f7dee138f41cd97e9bbc4b3
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-06 12:48:35 +0900

    Add: Metadata page titles

---
+++ src/app/lib/meta/index.ts
@@ -0,0 +1,11 @@
+// Root Page title
+export const Title = 'Home';
+
+// News Page title
+export const NewsTitle = 'News';
+
+// Tags Page title
+export const TagsTitle = 'Tags';
+
+// Contact Page title
+export const ContactTitle = 'Contact';
