c04161cf28f35a2ced4ee0c202e5f4bcb760db4e
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-07 01:27:27 +0900

    Add: email+pass認証

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,9 @@
 import type { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from "next-auth/providers/credentials"
+import { compare } from "bcrypt";
+
 
 export const options: NextAuthOptions = {
   providers: [
@@ -8,9 +11,27 @@ export const options: NextAuthOptions = {
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials ?? {}
+        if (!email || !password) {
+          throw new Error("Missing username or password");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        // if user doesn't exist or password doesn't match
+        if (!user || !(await compare(password, user.password))) {
+          throw new Error("Invalid username or password");
+        }
+        return user;
+      },
+    })
   ],
 };
