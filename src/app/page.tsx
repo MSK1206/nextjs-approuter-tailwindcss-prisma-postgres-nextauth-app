250047a2c715d89120abb1b2c21b36a90088c85f
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-06 12:45:52 +0900

    Add: Generate Metadata Open Graph protocol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,44 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { Metadata } from 'next';
+import { getMeta } from '@/app/lib/client/microcms';
+import { Title } from '@/app/lib/meta';
+
+export const revalidate = 0;
+
+export async function generateMetadata(): Promise<Metadata> {
+  const canonicalUrl = process.env.NEXT_PUBLIC_CANONICAL_URL as string;
+  const ogUrl = process.env.NEXT_PUBLIC_BASE_URL as string
+  const data = await getMeta();
+  if (!data) {
+    return {};
+  }
+
+  return {
+    metadataBase: new URL(
+      process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+    ),
+    title: `${Title} - ${data.titleTemplate}`,
+    description: data.description,
+    alternates: {
+      canonical: canonicalUrl,
+    },
+    openGraph: {
+      type: 'website',
+      url: ogUrl,
+      title: data.ogTitle,
+      description: data.ogDescription,
+      siteName: data.titleTemplate,
+      images: [
+        `${data?.ogImage?.url}?txt=${data.ogTitle}&txt-size=45&txt-align=middle,center&txt-shad=5` ||
+          '',
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+    },
+  };
+}
 
 export default function Home() {
   return (
