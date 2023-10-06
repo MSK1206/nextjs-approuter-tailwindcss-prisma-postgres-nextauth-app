58044f5ee8d54ae37195641b431e93f822a305f0
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-06 12:46:45 +0900

    Add: Pagination機能

---
+++ src/app/components/ui/Pagination/index.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import { LIMIT } from '@/app/lib/constants/limit';
+
+type Props = {
+  totalCount: number;
+  current?: number;
+  basePath?: string;
+  q?: string;
+};
+
+export default function Pagination({
+  totalCount,
+  current = 1,
+  basePath = '',
+  q,
+}: Props) {
+  const pages = Array.from({
+    length: Math.ceil(totalCount / LIMIT),
+  }).map((_, i) => i + 1);
+  return (
+    <ul className="flex items-center justify-center text-2xl">
+      {pages.map((p) => (
+        <li
+          key={p}
+          className="border border-solid pt-1 pb-1 pr-2 pl-2 first:rounded-l last:rounded-r"
+        >
+          {current !== p ? (
+            <Link href={`${basePath}/p/${p}` + (q ? `?q=${q}` : '')}>{p}</Link>
+          ) : (
+            <span className="text-blue-700">{p}</span>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+}
