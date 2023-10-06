6087ab2c72ae259c2fb070c8864328cd369c2338
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-06 10:57:21 +0900

    Add: remotePatterns and headers key & value

--- next.config.js
+++ next.config.js
@@ -1,4 +1,57 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io'
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/contact',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          }
+        ]
+      },
+      {
+        source: '/news',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      },
+      {
+        source: '/news/p/:current*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      },
+      {
+        source: '/news/:slug*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
