5d694e7570bbeeb9d8d1c8006309ab0070308ed0
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-06 21:40:32 +0900

    Add: mt-5

--- src/app/components/ui/Pagination/index.tsx
+++ src/app/components/ui/Pagination/index.tsx
@@ -18,7 +18,7 @@ export default function Pagination({
     length: Math.ceil(totalCount / LIMIT),
   }).map((_, i) => i + 1);
   return (
-    <ul className="flex items-center justify-center text-2xl">
+    <ul className="flex items-center justify-center text-2xl mt-5">
       {pages.map((p) => (
         <li
           key={p}
