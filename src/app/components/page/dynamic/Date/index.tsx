a269473bb7693ac12b8bf09d9470dea28708b534
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-06 20:45:44 +0900

    Add: 投稿日時のフォーマット

---
+++ src/app/components/page/dynamic/Date/index.tsx
@@ -0,0 +1,15 @@
+import { formatDate } from '@/app/lib/util/formatDate';
+import { ClockIcon } from '@heroicons/react/24/outline';
+
+type Props = {
+  date: string;
+};
+
+export default function PublishedDate({ date }: Props) {
+  return (
+    <span className="inline-flex items-center justify-around">
+      <ClockIcon className="h-4 w-4 text-black" />
+      {formatDate(date)}
+    </span>
+  );
+}
